1b8930b8c29bd8270950ac6ef57e8304d92f3bda
Lotfi <lotfi.bentaleb1995@gmail.com>
2021-04-14 12:05:12 -0700

    updated: made the home page clear

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -34,11 +34,6 @@ export default {
         'Production',
         [
           {
-            to: '/production/index',
-            label: 'Current production',
-            icon: 'crane',
-          },
-          {
             to: '/products/list',
             label: 'Products',
             icon: 'package-variant-closed',
@@ -49,63 +44,10 @@ export default {
         [
           {
             to: '/clients/index',
-            label: 'Clients',
-            icon: 'account-multiple',
-            updateMark: true
-          },
-          {
-            to: '/users/index',
             label: 'Users',
             icon: 'account-multiple',
             updateMark: true
           },
-
-        ],
-        'Examples',
-        [
-          {
-            to: '/tables',
-            label: 'Tables',
-            icon: 'table'
-          },
-          {
-            to: '/forms',
-            label: 'Forms',
-            icon: 'square-edit-outline'
-          },
-          {
-            to: '/profile',
-            label: 'Profile',
-            icon: 'account-circle'
-          },
-          {
-            label: 'Submenus',
-            subLabel: 'Submenus Example',
-            icon: 'view-list',
-            menu: [
-              {
-                href: '#void',
-                label: 'Sub-item One'
-              },
-              {
-                href: '#void',
-                label: 'Sub-item Two'
-              }
-            ]
-          }
-        ],
-        'About',
-        [
-          {
-            href: 'https://admin-one-laravel.justboil.me',
-            label: 'Premium Demo',
-            icon: 'credit-card'
-          },
-          {
-            href: 'https://justboil.me/bulma-admin-template/one',
-            label: 'About',
-            icon: 'help-circle'
-          }
         ]
       ]
     }
--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -17,29 +17,6 @@
     </div>
     <div class="navbar-menu fadeIn animated faster" :class="{'is-active':isMenuNavBarActive}">
       <div class="navbar-end">
-        <nav-bar-menu class="has-divider">
-          <b-icon icon="menu" custom-size="default"/>
-          <span>New Sample Menu</span>
-          <div slot="dropdown" class="navbar-dropdown">
-            <router-link to="/profile" class="navbar-item" exact-active-class="is-active">
-              <b-icon icon="account" custom-size="default"/>
-              <span>My Profile</span>
-            </router-link>
-            <a class="navbar-item">
-              <b-icon icon="settings" custom-size="default"/>
-              <span>Settings</span>
-            </a>
-            <a class="navbar-item">
-              <b-icon icon="email" custom-size="default"/>
-              <span>Messages</span>
-            </a>
-            <hr class="navbar-divider">
-            <a class="navbar-item">
-              <b-icon icon="logout" custom-size="default"/>
-              <span>Log Out</span>
-            </a>
-          </div>
-        </nav-bar-menu>
         <nav-bar-menu class="has-divider has-user-avatar">
           <user-avatar/>
           <div class="is-user-name">
@@ -47,7 +24,7 @@
           </div>
 
           <div slot="dropdown" class="navbar-dropdown">
-            <a class="navbar-item">
+            <a class="navbar-item" href="/#/profile">
               <b-icon icon="account" custom-size="default"></b-icon>
               <span>My Profile</span>
             </a>
@@ -55,12 +32,8 @@
               <b-icon icon="settings" custom-size="default"></b-icon>
               <span>Settings</span>
             </a>
-            <a class="navbar-item">
-              <b-icon icon="email" custom-size="default"></b-icon>
-              <span>Messages</span>
-            </a>
             <hr class="navbar-divider">
-            <a class="navbar-item">
+            <a class="navbar-item" @click="logout">
               <b-icon icon="logout" custom-size="default"></b-icon>
               <span>Log Out</span>
             </a>
