bb724ad642a57316ac83bb85a8af44778476c751
Lotfi <lotfi.bentaleb1995@gmail.com>
2021-04-28 00:53:22 -0700

    fixed: the check modal text

--- resources/js/components/ProductsTemplate/ModalUpdateCheck.vue
+++ resources/js/components/ProductsTemplate/ModalUpdateCheck.vue
@@ -1,16 +1,11 @@
 <template>
-  <!--<modal-box :is-active.sync="isActive" @confirm="confirm" @cancel="cancel" confirm-type="is-danger" confirm-label="Cancel">-->
-    <!--<p>Dauerhaftes Löschen von <b>{{ modalSubject }}</b>?</p>-->
-    <!--<p>Diese Aktion kann nicht rückgängig gemacht werden</p>-->
-  <!--</modal-box>-->
-
   <b-modal :active="isActive" has-modal-card>
     <div class="modal-card section-template-modal">
       <header class="modal-card-head">
         <p class="modal-card-title">Modal to check the changed data</p>
       </header>
       <section class="modal-card-body">
-        <p>You must save the changed data before selecting data</p>
+        <p>You must save the changed data before leaving this row.</p>
       </section>
       <footer class="modal-card-foot custom-foot">
         <b-button class="btn btn-ok"  @click="confirm">Save</b-button>
@@ -18,7 +13,6 @@
       </footer>
     </div>
   </b-modal>
-
 </template>
 
 <script>
