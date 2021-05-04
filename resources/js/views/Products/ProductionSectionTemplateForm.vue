4f906e7ab25ff0f80cd9ee1c9864066afa041068
Lotfi <lotfi.bentaleb1995@gmail.com>
2021-05-04 00:28:22 -0700

    added: a new page for adding a new production section template

---
+++ resources/js/views/Products/ProductionSectionTemplateForm.vue
@@ -0,0 +1,194 @@
+<template>
+  <div>
+    <title-bar :title-stack="titleStack"/>
+    <hero-bar>
+      {{ heroTitle }}
+      <router-link slot="right" to="/products/section/template" class="button">
+        Section Templates
+      </router-link>
+    </hero-bar>
+    <create-section-template-modal :is-json-modal="isJsonModal" @addedJsonData="addedJsonData" @cancelJsonAdd="cancelJsonAdd"></create-section-template-modal>
+    <section class="section is-main-section">
+      <tiles>
+        <card-component :title="formCardTitle" icon="account-edit" class="tile is-child">
+          <form @submit.prevent="submit">
+            <b-field label="Name" message="production section template name">
+              <b-input placeholder="e.g. measurement.daisy" v-model="form.name" required expanded/>
+            </b-field>
+            <b-field label="Darstellung">
+              <b-select placeholder="default" v-model="form.render_hint" required expanded>
+                <option v-for="(render_hints, index) in formHelper.render_hints" :key="index" :value="render_hints">
+                  {{ render_hints }}
+                </option>
+              </b-select>
+            </b-field>
+            <b-field label="Gruppe">
+              <b-select placeholder="default" v-model="form.group" required expanded>
+                <option v-for="(groups, index) in formHelper.groups" :key="index" :value="groups">
+                  {{ groups }}
+                </option>
+              </b-select>
+            </b-field>
+            <b-field label="Beschreibung" message="Description with using up to 255 characters" expanded>
+              <b-input type="textarea" placeholder="Explain how we can help you" v-model="form.description" maxlength="255" required/>
+            </b-field>
+            <b-field expanded>
+              <b-button v-if="!isJsonEmpty" @click="clickedAddJsonBtn">Add data</b-button>
+              <b-button v-else type="is-danger" @click="clickedAddJsonBtn">Add data</b-button>
+            </b-field>
+            <div class="level">
+              <div class="level-left">
+              </div>
+              <div class="level-right">
+                <b-field >
+                  <b-button class="btn btn-ok" :loading="isLoading" native-type="submit">Submit</b-button>
+                </b-field>
+              </div>
+            </div>
+          </form>
+        </card-component>
+        <card-component v-if="hasJsonItem" title="Produktions-Ablauf" icon="account" class="tile is-child">
+          <b-field label="Produktions-Ablauf" message="Produktions-Ablauf" >
+            <v-jsoneditor v-model="jsonData" />
+          </b-field>
+          <hr>
+        </card-component>
+      </tiles>
+    </section>
+  </div>
+</template>
+
+<script>
+  import clone from 'lodash/clone'
+  import TitleBar from '@/components/TitleBar'
+  import HeroBar from '@/components/HeroBar'
+  import Tiles from '@/components/Tiles'
+  import CardComponent from '@/components/CardComponent'
+  import FilePicker from '@/components/FilePicker'
+  import UserAvatar from '@/components/UserAvatar'
+  import Notification from '@/components/Notification'
+  import BField from "buefy/src/components/field/Field"
+  import VJsoneditor from 'v-jsoneditor'
+  import CreateSectionTemplateModal from '@/components/ProductionSectionTemplate/CreateSectionTemplateModal'
+
+  export default {
+    name: 'ProductionSectionTemplateForm',
+    components: {BField, UserAvatar, FilePicker, CardComponent, VJsoneditor, CreateSectionTemplateModal, Tiles, HeroBar, TitleBar, Notification },
+    props: {
+      id: {
+        default: null
+      }
+    },
+    data () {
+      return {
+        isLoading: false,
+        isJsonModal: false,
+        hasJsonItem: false,
+        item: null,
+        form: this.getClearFormObject(),
+        createdReadable: null,
+        formHelper: [],
+        jsonData: [],
+        isJsonEmpty: false,
+      }
+    },
+    computed: {
+      titleStack () {
+        return [
+          'Products',
+          'Section',
+          'New Section Template'
+        ]
+      },
+      heroTitle () {
+        return 'Create Section Template'
+      },
+      formCardTitle () {
+        return 'New Section Template'
+      }
+    },
+    created () {
+      this.updateFormHelper()
+    },
+    methods: {
+      clickedAddJsonBtn() {
+        this.isJsonModal = true
+      },
+      addedJsonData(data) {
+        this.hasJsonItem = true
+        this.isJsonModal = false
+        this.jsonData.push(data)
+        this.isJsonEmpty = false
+      },
+      cancelJsonAdd() {
+        this.isJsonModal = false
+      },
+      getClearFormObject () {
+        return {
+          name: null,
+          render_hint: null,
+          group: null,
+          description: null,
+        }
+      },
+      updateFormHelper() {
+        axios.get(`/production_section/form_support`, {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+            .then( result => {
+              this.formHelper = result.data;
+              console.log(this.formHelper)
+            })
+            .catch( error => {
+              console.log('Error', error.message);
+            })
+            .finally(() => {
+            })
+      },
+      submit () {
+        if(this.jsonData.length == 0) {
+          this.isJsonEmpty = true
+          this.$buefy.snackbar.open({
+            type: 'is-danger',
+            message: 'Empty data field, Please add data',
+            queue: false
+          })
+          return
+        }
+        this.isLoading = true
+        let method = 'post'
+        let url = '/production_section'
+        let data = {
+          name: this.form.name,
+          description: this.form.description,
+          group: this.form.group,
+          data:JSON.stringify(this.jsonData),
+          render_hint: this.form.render_hint
+        }
+        axios({
+          method,
+          url,
+          data: data
+        }).then(r => {
+          this.isLoading = false
+          if (r.data ) {
+            this.$router.push({name: 'products.section_template'})
+            this.$buefy.snackbar.open({
+              message: 'Created New Section Template',
+              queue: false
+            })
+          }
+        }).catch(e => {
+          this.isLoading = false
+          this.$buefy.toast.open({
+            message: `Error: ${e.message}`,
+            type: 'is-danger',
+            queue: false
+          })
+        })
+      }
+    }
+  }
+</script>
