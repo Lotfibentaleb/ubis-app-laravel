ad4d1bee86a8032557c1bda7b04895a1c633725c
Lotfi <lotfi.bentaleb1995@gmail.com>
2021-05-21 12:30:42 -0700

    updated: the home page for the dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use GuzzleHttp;
-
 
 class DashboardController extends Controller
 {
@@ -37,6 +37,33 @@ class DashboardController extends Controller
         $options = [
             'headers' =>[
                 'Authorization' => 'Bearer ' .env('PIS_BEARER_TOKEN'),
+                'Accept'        => 'application/json',
+                'Content-Type' => 'application/json'
+            ],
+        ];
+        $response = $client->request('get', $baseUrl.$requestString, $options);   // call API
+        $statusCode = $response->getStatusCode();
+        $body = json_decode($response->getBody()->getContents());
+        return response()->json($body, $statusCode);
+    }
+
+    public function homeDashboard(Request $request)
+    {
+        $paramDate = $request->input('selectedDate', '');
+        $client = new GuzzleHttp\Client();
+        $baseUrl = env('PIS_SERVICE_BASE_URL2');
+        $requestString = isset($paramDate) ? 'dashboard/info?selectedDate='.$paramDate : 'dashboard/info';
+
+        $bearer_token = '';
+        if (Session::has('bearer_token')) {
+            $bearer_token = Session::get('bearer_token');
+        } else {
+            return redirect('login');
+        }
+
+        $options = [
+            'headers' =>[
+                'Authorization' => 'Bearer ' .$bearer_token,
                 'Accept'        => 'application/json',
                 'Content-Type' => 'application/json'
             ],
