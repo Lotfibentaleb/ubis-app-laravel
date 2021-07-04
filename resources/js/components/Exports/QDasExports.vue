b1ee3560302f88adbaf11249d6a5858b373651f0
Lotfi <lotfi.bentaleb1995@gmail.com>
2021-07-04 10:11:16 -0700

    updated: Q-DAS export component so that showing date range information

--- resources/js/components/Exports/QDasExports.vue
+++ resources/js/components/Exports/QDasExports.vue
@@ -1,5 +1,24 @@
 <template>
   <card-component :title="$gettext('exportsPage.qdasExports.card.title')" icon="package-variant-closed" class="tile is-child">
+    <div class="level">
+      <div class="level-left"></div>
+      <div class="level-right">
+        <b-icon
+                icon="calendar-today">
+        </b-icon>
+        <date-range-picker
+                ref="picker"
+                :timePicker="timePicker"
+                :timePicker24Hour="timePicker24Hour"
+                v-model="dateRange"
+                @update="updateDateRange"
+        >
+          <template v-slot:input="picker" style="min-width: 350px;">
+            {{ picker.startDate | moment("DD.MM.YYYY / k:mm:ss") }} - {{ picker.endDate | moment("DD.MM.YYYY / k:mm:ss") }}
+          </template>
+        </date-range-picker>
+      </div>
+    </div>
     <div class="column is-one-fifths is-offset-two-fifths">
       <p>{{$gettext('exportsPage.qdasExports.explanation')}}</p>
     </div>
@@ -11,13 +30,42 @@
 
 <script>
   import CardComponent from '@/components/CardComponent'
+  import DateRangePicker from 'vue2-daterange-picker'
+  import 'vue2-daterange-picker/dist/vue2-daterange-picker.css'
+
   export default {
     name: 'QDasExports',
-    components: {CardComponent},
+    components: {CardComponent, DateRangePicker},
+    data() {
+      const today = new Date()
+      return {
+        dateRange: {
+          startDate: new Date(today.getFullYear() - 1, today.getMonth(), today.getDate()),
+          endDate: today,
+        },
+        dateRangeValues: '{}',
+        timePicker: true,
+        timePicker24Hour: true,
+      }
+    },
+    filters: {
+      dateCell (value) {
+        let dt = new Date(value)
+
+        return dt.getDate()
+      },
+      date (val) {
+        return val ? val.toLocaleString() : ''
+      }
+    },
     methods: {
       qdisExports() {
 
-      }
+      },
+      updateDateRange() {
+        this.dateRangeValues = ''
+        this.dateRangeValues = encodeURIComponent(JSON.stringify(this.dateRange))
+      },
     }
   }
 </script>
