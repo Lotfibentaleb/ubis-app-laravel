245e2d2cf82230e855df7e9b13adca00a3255c32
Lotfi <lotfi.bentaleb1995@gmail.com>
2021-07-04 12:02:54 -0700

    updated: full excel export feature on Exports page so that using Article Nr and Production Order-Nr

--- resources/js/components/Exports/ExcelExports.vue
+++ resources/js/components/Exports/ExcelExports.vue
@@ -22,6 +22,40 @@
     <div class="column is-one-fifths is-offset-two-fifths">
       <p>{{$gettext('exportsPage.excelExports.explanation')}}</p>
     </div>
+    <div class="column is-12">
+      <b-field :label="$gettext('bulkRegisterPage.articleNr')" :message="$gettext('bulkRegisterPage.articleNr.message')" horizontal>
+        <b-autocomplete
+                :data="articleList"
+                v-model="articleNr"
+                placeholder="Nach ERP Artikelnummer suchen z.B. 800000114B2"
+                :loading="isFetchingArticleList"
+                icon="magnify"
+                field="articleNumber"
+                :clearable="clearable"
+                @typing="getAsyncArticleList"
+                @select="option => articleSelected = option"
+                size="is-medium"
+                required
+        >
+          <template slot-scope="props">
+            <div class="media">
+              <div class="media-content">
+                {{ props.option.articleNumber }}<br>
+                <small>
+                  <div v-show="props.option.productionArticle == true">- Produktions Artikel -</div>
+                  {{ props.option.name }}
+                </small>
+              </div>
+            </div>
+          </template>
+        </b-autocomplete>
+      </b-field>
+    </div>
+    <div class="column is-12">
+      <b-field :label="$gettext('bulkRegisterPage.productionOrderNr')" :message="$gettext('bulkRegisterPage.productionOrderNr.message')" horizontal>
+        <b-input placeholder="e.g. 2021-1446" v-model="prodOrderNr" required />
+      </b-field>
+    </div>
     <div class="column is-1 is-offset-11">
       <a :href="hrefUrl"><b-button :label="$gettext('exportsPage.excelExports.exportBtn')" type="is-primary" /></a>
     </div>
@@ -30,9 +64,10 @@
 
 <script>
   import CardComponent from '@/components/CardComponent'
-  import BButton from "buefy/src/components/button/Button";
+  import BButton from "buefy/src/components/button/Button"
   import DateRangePicker from 'vue2-daterange-picker'
   import 'vue2-daterange-picker/dist/vue2-daterange-picker.css'
+  import debounce from 'lodash/debounce'
 
   export default {
     name: 'ExcelExports',
@@ -52,6 +87,15 @@
         sortField:'created_at',
         sortOrder:'desc',
         filterValues: '{}',
+
+        //auto complete
+        articleNr: '',
+        articleList: [],
+        articleSelected: null,
+        isFetchingArticleList: false,
+        clearable: false,
+
+        prodOrderNr: '',
       }
     },
     created () {
@@ -65,6 +109,14 @@
       },
       date (val) {
         return val ? val.toLocaleString() : ''
+      }
+    },
+    watch: {
+      articleNr: function() {
+        this.setFilterValues()
+      },
+      prodOrderNr: function() {
+        this.setFilterValues()
       }
     },
     methods: {
@@ -82,7 +134,74 @@
           `filter=${this.filterValues}`
         ].join('&')
         this.hrefUrl = this.dataUrl + '/fullExcel?' + paramsFullExcel
-      }
+      },
+      setFilterValues() {
+        let objData = {}
+        if(this.articleNr != '') {
+          if(this.prodOrderNr != '') {
+            objData = {
+              production_order_nr: this.prodOrderNr,
+              st_article_nr: this.articleNr
+            }
+          }else {
+            objData = {
+              st_article_nr: this.articleNr
+            }
+          }
+        }else {
+          if(this.prodOrderNr != '') {
+            objData = {
+              production_order_nr: this.prodOrderNr
+            }
+          }
+        }
+        this.filterValues = '';
+        this.filterValues = encodeURIComponent(JSON.stringify(objData));
+        this.getHrefURL()
+      },
+      getAsyncArticleList: debounce(function (name) {
+            if (!name.length) {
+              this.articleList = []
+              return
+            }
+            this.isFetchingArticleList = true
+
+            axios
+                .get(`/registration/articles?search_artnr=${name}`, {
+                  headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+                    "Access-Control-Allow-Headers": "Authorization, Content-Type, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset, Retry-After, DNT, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Range"
+                  }
+                })
+                .then( result => {
+                  console.log('Article list');
+                  console.log(result.data)
+                  this.isFetchingArticleList = false
+                  this.articleList = []
+                  result.data.data.forEach((item) => this.articleList.push(item))
+                })
+                .catch( error => {
+                  if (error.request) {
+                    // The request was made but no response was received, `error.request` is an instance of XMLHttpRequest in the browser and an instance
+                    // of http.ClientRequest in Node.js
+                    console.log(error.request);
+                  }else  if (error.response) {
+                    // The request was made and the server responded with a status code that falls out of the range of 2xx
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                  } else {
+                    // Something happened in setting up the request and triggered an Error
+                    console.log('Error', error.message);
+                  }
+                })
+                .finally(() => {
+                  this.isFetchingArticleList = false
+                })
+          },
+          250),
     }
   }
 </script>
