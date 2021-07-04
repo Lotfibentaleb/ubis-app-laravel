e6cb2e910ab6674e95c5d2fdd232ba3b62d6bc43
Lotfi <lotfi.bentaleb1995@gmail.com>
2021-07-04 10:03:34 -0700

    updated: full excel export feature on the export page so that using date range information

--- resources/js/components/Exports/ExcelExports.vue
+++ resources/js/components/Exports/ExcelExports.vue
@@ -1,5 +1,24 @@
 <template>
   <card-component :title="$gettext('exportsPage.excelExports.card.title')" icon="package-variant-closed" class="tile is-child">
+    <div class="level">
+      <div class="level-left"></div>
+      <div class="level-right">
+        <b-icon
+                icon="calendar-today">
+        </b-icon>
+        <date-range-picker
+                ref="picker"
+                :timePicker="timePicker"
+                :timePicker24Hour="timePicker24Hour"
+                v-model="dateRange"
+                @update="updateDateRange"
+        >
+          <template v-slot:input="picker" style="min-width: 350px;">
+            {{ picker.startDate | moment("DD.MM.YYYY / k:mm:ss") }} - {{ picker.endDate | moment("DD.MM.YYYY / k:mm:ss") }}
+          </template>
+        </date-range-picker>
+      </div>
+    </div>
     <div class="column is-one-fifths is-offset-two-fifths">
       <p>{{$gettext('exportsPage.excelExports.explanation')}}</p>
     </div>
@@ -12,11 +31,22 @@
 <script>
   import CardComponent from '@/components/CardComponent'
   import BButton from "buefy/src/components/button/Button";
+  import DateRangePicker from 'vue2-daterange-picker'
+  import 'vue2-daterange-picker/dist/vue2-daterange-picker.css'
+
   export default {
     name: 'ExcelExports',
-    components: {BButton, CardComponent},
+    components: {BButton, CardComponent, DateRangePicker},
     data() {
+      const today = new Date()
       return {
+        dateRange: {
+          startDate: new Date(today.getFullYear() - 1, today.getMonth(), today.getDate()),
+          endDate: today,
+        },
+        dateRangeValues: '{}',
+        timePicker: true,
+        timePicker24Hour: true,
         hrefUrl: '',
         dataUrl: '/productlist',
         sortField:'created_at',
@@ -27,12 +57,28 @@
     created () {
       this.getHrefURL()
     },
+    filters: {
+      dateCell (value) {
+        let dt = new Date(value)
+
+        return dt.getDate()
+      },
+      date (val) {
+        return val ? val.toLocaleString() : ''
+      }
+    },
     methods: {
+      updateDateRange() {
+        this.dateRangeValues = '';
+        this.dateRangeValues = encodeURIComponent(JSON.stringify(this.dateRange));
+        this.getHrefURL()
+      },
       getHrefURL() {
         const paramsFullExcel = [
           `enhanced=2`,
           `sort_by=${this.sortField}.${this.sortOrder}`,
           `page=${this.page}`,
+          `date_range=${this.dateRangeValues}`,
           `filter=${this.filterValues}`
         ].join('&')
         this.hrefUrl = this.dataUrl + '/fullExcel?' + paramsFullExcel
