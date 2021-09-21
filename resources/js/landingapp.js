d137af58988b840144921a9a579e1afea291096b
Lotfi <lotfi.bentaleb1995@gmail.com>
2021-09-20 22:58:10 -0700

    updated: landing page

--- resources/js/landingapp.js
+++ resources/js/landingapp.js
@@ -11,15 +11,27 @@ import router from './landingapprouter'
 /* Vue. Main component */
 import App from './LandingApp.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+// internal icons
+import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faPhoneAlt);
+
 //import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false
 
 /* Main component */
 Vue.component('LandingApp', App)
-/* Buefy */
-Vue.use(Buefy)
 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+/* Buefy */
+Vue.use(Buefy, {
+  defaultIconComponent: 'vue-fontawesome',
+  defaultIconPack: 'fas',
+});
 /* This is main entry point */
 
 new Vue({
--- resources/js/views/LandingPage.vue
+++ resources/js/views/LandingPage.vue
@@ -1,8 +1,241 @@
 <template>
-  <div>
-    <h1>Hello Landing Page</h1>
+  <div class="landing-pg">
+    <div class="columns">
+      <div class="column">
+        <div class="landing-header">
+          <img class="img-logo" src="images/brand.svg" />
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <div class="carousel-area">
+          <div class="columns">
+            <div class="column item-center">
+              <h1 class="label-ubis">UBIS</h1>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column item-center">
+              <p class="label-service">Integrated Manufacturing Services</p>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column item-center">
+              <div class="up-arch-svg">
+                <svg viewBox="0 0 2880 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M720 28H0v28h2880V28h-720S1874 0 1440 0 720 28 720 28z" fill="currentColor"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="columns card-container">
+      <div class="container">
+        <div class="columns is-mobile">
+          <div class="column is-6">
+            <div class="card-item">
+              <div class="columns">
+                <div class="column">
+                  <div class="icon-label item-center">
+                    <img class="card-icon" src="images/registration-icon.svg" />
+                    <p class="card-label">Product registration</p>
+                  </div>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column">
+                  <div class="card-btn">
+                    <p class="card-btn-label">Open</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column is-6">
+            <div class="card-item">
+              <div class="columns">
+                <div class="column">
+                  <div class="icon-label item-center">
+                    <img class="card-icon" src="images/search-icon.svg" />
+                    <p class="card-label">Product search</p>
+                  </div>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column">
+                  <div class="card-btn">
+                    <p class="card-btn-label">Open</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="columns is-mobile">
+          <div class="column is-6">
+            <div class="card-item">
+              <div class="columns">
+                <div class="column">
+                  <div class="icon-label item-center">
+                    <img class="card-icon" src="images/configuration-icon.svg" />
+                    <p class="card-label">Configuration</p>
+                  </div>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column">
+                  <div class="card-btn">
+                    <p class="card-btn-label">Open</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column is-6">
+            <div class="card-item">
+              <div class="columns">
+                <div class="column">
+                  <div class="icon-label item-center">
+                    <img class="card-icon" src="images/dashboard-icon.svg" />
+                    <p class="card-label">Dashboard</p>
+                  </div>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column">
+                  <div class="card-btn">
+                    <p class="card-btn-label">Open</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="contact-container">
+      <div class="columns">
+        <div class="column item-center">
+          <div class="down-arch-svg">
+            <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 48h2880V0h-720C1442.5 52 720 0 720 0H0v48z" fill="currentColor"></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="columns">
+              <img class="contact-logo" src="images/brand.svg" />
+            </div>
+            <div class="columns">
+              <p class="contact-sub-label">We Display your Brilliance.</p>
+            </div>
+            <div class="columns">
+              <div class="column is-7">
+                <div class="columns social-links">
+                  <div class="column">
+                    <img src="images/instagram.svg" />
+                  </div>
+                  <div class="column">
+                    <img src="images/facebook.svg" />
+                  </div>
+                  <div class="column">
+                    <img src="images/twitter.svg" />
+                  </div>
+                  <div class="column">
+                    <img src="images/pinterest.svg" />
+                  </div>
+                </div>
+              </div>
+              <div class="column is-5"></div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="columns">
+              <p class="contact-main-label">INTERNAL LINKS</p>
+            </div>
+            <div class="columns">
+              <a href="#" class="contact-sub-label">Sharepoint</a>
+            </div>
+            <div class="columns">
+              <a href="#" class="contact-sub-label">Outlook</a>
+            </div>
+            <div class="columns">
+              <a href="#" class="contact-sub-label">etc ...</a>
+            </div>
+          </div>
+          <div class="column">
+            <div class="columns">
+              <p class="contact-main-label">EXTERNAL LINKS</p>
+            </div>
+            <div class="columns">
+              <a href="#" class="contact-sub-label">Semsotec Website</a>
+            </div>
+            <div class="columns">
+              <a href="#" class="contact-sub-label">UBIS Docs</a>
+            </div>
+            <div class="columns">
+              <a href="#" class="contact-sub-label">etc ...</a>
+            </div>
+          </div>
+          <div class="column">
+            <div class="columns">
+              <p class="contact-main-label">SUPPORT CONTACT</p>
+            </div>
+            <div class="columns">
+              <a class="contact-sub-label" href="#">Technical support</a>
+            </div>
+            <div class="columns">
+              <p class="contact-owner"><strong>Max Mustermann</strong></p>
+            </div>
+            <div class="columns">
+              <!--<b-icon-->
+                <!--pack="fas"-->
+                <!--icon="faPhoneAlt"-->
+                <!--size="is-large"-->
+                <!--type="is-success"></b-icon>-->
+              <p class="contact-phone">+49 1234 56789</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-container">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <img class="footer-logo-icon" src="images/logo-fast-solutions.svg" />
+          </div>
+          <div class="column">
+            <img class="footer-logo-icon" src="images/logo-esp.svg" />
+          </div>
+          <div class="column">
+            <img class="footer-logo-icon" src="images/logo-iligenta.svg" />
+          </div>
+          <div class="column">
+            <img class="footer-logo-icon" src="images/logo-corporation.svg" />
+          </div>
+          <div class="column">
+            <img class="footer-logo-icon" src="images/logo-czech.svg" />
+          </div>
+          <div class="column">
+            <img class="footer-logo-icon" src="images/logo-holding.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-
+  import Parallax from "vue-parallaxy";
+  export default {
+    components: {
+      Parallax
+    }
+  };
 </script>
