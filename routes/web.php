30cf7010f5425c39bf7a437cc01b91e3d35bbd44
Lotfi <lotfi.bentaleb1995@gmail.com>
2021-09-21 23:25:46 -0700

    updated: landing page router so that working with both of / and /landing

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'LandingController@index')->name('landing');
 
 Auth::routes();
 
